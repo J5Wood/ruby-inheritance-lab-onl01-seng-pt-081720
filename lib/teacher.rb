f1aebffa0fad1f151e522abdbbb02252978d6d02
Jeremy Wood <js.wood206@gmail.com>
2020-09-15 04:35:40 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,10 +1,16 @@
 class Teacher < User
 
-  attr_accessor :student
+  attr_accessor :student, :classes
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  def initialize
+    classes = []
+  end
 
+  def add_class(class_to_add)
+    class_to_add
+    
   def teach
     KNOWLEDGE.sample
   end
