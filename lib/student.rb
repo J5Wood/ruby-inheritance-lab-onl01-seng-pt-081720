9daf4c06c7bd30ed943a0b9f2ab522a06b989073
Jeremy Wood <js.wood206@gmail.com>
2020-09-15 04:52:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,6 @@ class Student < User
 
   attr_accessor :knowledge, :teacher, :classes
 
-  def sign_up_for_class(class_desired)
-    CLASSES.find do |classes|
-      if classes.upcase == class_desired.upcase
-        @classes << classes
-        "You have signed up for #{classes}"
-      else
-        "You did not enter a valid class"
-      end
-    end
-  end
-
 
 
   def initialize
@@ -28,7 +17,15 @@ class Student < User
      self.freeze
   end
 
-
-
+  def sign_up_for_class(class_desired)
+    CLASSES.find do |classes|
+      if classes.upcase == class_desired.upcase
+        @classes << classes
+        "You have signed up for #{classes}"
+      else
+        "You did not enter a valid class"
+      end
+    end
+  end
 
 end
